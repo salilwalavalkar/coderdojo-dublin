e6d6ed2b83d6c8eb4d0b1e6d424c5d940832ed82
Alexandre Fontoura <xesfnet@gmail.com>
2023-08-11 09:26:12 +0100

    feat: Simplify robot interactions
    Co-authored-by: Alexandre Fontoura <alexandre.fontoura@viasat.com>

---
+++ robot/robot_police.py
@@ -0,0 +1,40 @@
+# Police Robot
+
+# 1. Turn On the Police Lights
+# 2. Make a Police Siren Sound
+
+from robot import *
+
+turn_on()
+
+message('Robot Police!!')
+
+#while(RUNNING):
+for r in range(0, 10):
+    use_joystick()
+
+    # get distance
+    value = distance()
+    message(f'Distance: {value} cm', row = 2)
+
+    # siren sound and lights
+    lights_on(BLUE)
+    sound(1000, 800, 0.2)
+    lights_on(RED)
+    sound(600, 800, 0.2)
+
+lights_off()
+
+# spin the robot
+#clear_message()
+#message('Spinning...')
+#message('...........', row = 2)
+#spin()
+#wait(2)
+
+clear_message()
+message('Shutting Down!!')
+wait(2)
+
+turn_off()
+
